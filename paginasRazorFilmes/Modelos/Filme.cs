fca3c682528420d727c0c5fc2b8a20db6500fa22
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2021-03-06 19:00:52 -0300

    :tada: - Adiciona classe de modelo "Filme"

---
+++ paginasRazorFilmes/Modelos/Filme.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace paginasRazorFilmes.Modelos
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DataDeLancamento { get; set; }
+        public string Genero { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
