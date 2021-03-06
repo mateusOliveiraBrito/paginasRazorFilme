d0edd8c7c8c0be2f4e8a2052ffdaca4e96d2a9fd
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2021-03-06 19:16:12 -0300

    :tada: - Cria esquema de banco de dados Utiliza o recurso de migração do EntityFramework para criar o esquema inicial do banco de dados com base na classe de modelo criada (Filme)

---
+++ paginasRazorFilmes/Migrations/20210306221245_paginasRazorFilmeManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace paginasRazorFilmes.Migrations
+{
+    public partial class paginasRazorFilmeManager : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Filme",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DataDeLancamento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Genero = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Preco = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Filme", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Filme");
+        }
+    }
+}
