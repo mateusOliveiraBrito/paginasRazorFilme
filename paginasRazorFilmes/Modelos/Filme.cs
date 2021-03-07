82c63b5a2549e453d05fb4b6ebc19f14a399b7c5
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2021-03-07 11:45:09 -0300

    :art: - Adiciona validação de campos na model e remove validação JQuery no lado cliente servidor

--- paginasRazorFilmes/Modelos/Filme.cs
+++ paginasRazorFilmes/Modelos/Filme.cs
@@ -7,15 +7,28 @@ namespace paginasRazorFilmes.Modelos
     public class Filme
     {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Titulo { get; set; }
 
         [Display(Name = "Data de Lançamento")]
         [DataType(DataType.Date)]
         public DateTime DataDeLancamento { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genero { get; set; }
 
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Preco { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string Classificacao { get; set; }
     }
 }
--- paginasRazorFilmes/Startup.cs
+++ paginasRazorFilmes/Startup.cs
@@ -25,7 +25,11 @@ namespace paginasRazorFilmes
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages()
+                .AddViewOptions(options =>
+                {
+                    options.HtmlHelperOptions.ClientValidationEnabled = false;
+                });
 
             services.AddDbContext<paginasRazorFilmesContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("paginasRazorFilmesContext")));
