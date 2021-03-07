dd6fa22fdb318f5e990ab81521680a2067f589da
Mateus Oliveira Brito <moliveirabrito.institucional@gmail.com>
2021-03-07 10:37:08 -0300

    :art: - Adiciona filtros de pesquisa

--- paginasRazorFilmes/Pages/Filmes/Index.cshtml.cs
+++ paginasRazorFilmes/Pages/Filmes/Index.cshtml.cs
@@ -1,29 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using paginasRazorFilmes.Data;
 using paginasRazorFilmes.Modelos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace paginasRazorFilmes.Pages.Filmes
 {
     public class IndexModel : PageModel
     {
-        private readonly paginasRazorFilmes.Data.paginasRazorFilmesContext _context;
+        private readonly paginasRazorFilmesContext _context;
 
-        public IndexModel(paginasRazorFilmes.Data.paginasRazorFilmesContext context)
+        public IndexModel(paginasRazorFilmesContext context)
         {
             _context = context;
         }
 
-        public IList<Filme> Filme { get;set; }
+        public IList<Filme> Filme { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string StringDePesquisa { get; set; }
+        public SelectList Generos { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string GeneroFilme { get; set; }
 
         public async Task OnGetAsync()
         {
-            Filme = await _context.Filme.ToListAsync();
+            IQueryable<string> queryGenero = from f in _context.Filme
+                                             orderby f.Genero
+                                             select f.Genero;
+
+            var filmes = from f in _context.Filme
+                         select f;
+
+            if (!string.IsNullOrEmpty(StringDePesquisa))
+            {
+                filmes = filmes.Where(s => s.Titulo.Contains(StringDePesquisa));
+            }
+
+            if (!string.IsNullOrEmpty(GeneroFilme))
+            {
+                filmes = filmes.Where(x => x.Genero == GeneroFilme);
+            }
+
+            Generos = new SelectList(await queryGenero.Distinct().ToListAsync());
+            Filme = await filmes.ToListAsync();
         }
     }
 }
